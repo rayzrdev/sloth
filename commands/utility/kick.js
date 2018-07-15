476c2a4ca39adb1cadb2c852fe6bf8e42a6c5ff0
Sushii <sushiidevelopment@gmail.com>
2018-07-15 17:00:25 +0200

    Fixed error.

--- commands/utility/kick.js
+++ commands/utility/kick.js
@@ -1,29 +1,29 @@
 exports.run = async (client, message, args) => {
-    const member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    const target = message.mentions.members.first() || message.guild.members.get(args[0]);
     const reason = args.slice(1).join(' ');
     
     if (args.length < 2) {
         throw ':x: Please provide both a user and a reason.';
     }
     
-    if (!member) {
+    if (!target) {
         throw ':x: Sorry, but that user could not be found.';
     }
     
-    if (!message.member.hasPermission('KICK_MEMBERS') {
+    if (!message.member.hasPermission('KICK_MEMBERS')) {
         throw ':x: Sorry, but requires the following permission node `KICK_MEMBERS`.'
     }
         
-    if (member.hasPermission('KICK_MEMBERS') {
+    if (target.hasPermission('KICK_MEMBERS')) {
         throw ':x: Sorry, but you may not kick a fellow staff member.'
     }
 
     if (reason) {
-        await member.send(`You were kicked for **${reason}**`);
+        await target.send(`You were kicked for **${reason}**`);
     }
     
-    member.kick(reason);
-    message.channel.send(`:white_check_mark: **${member.user.tag}** has been successfully kicked.`);
+    target.kick(reason);
+    message.channel.send(`:white_check_mark: **${target.user.tag}** has been successfully kicked.`);
 };
 
 exports.info = {
