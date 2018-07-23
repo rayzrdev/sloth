9d106c4f11fafdda564927d608445d286090a45e
RayzrDev <rayzr522@gmail.com>
2018-07-22 22:22:23 -0400

    Added biography support

--- src/commands/info/profile.js
+++ src/commands/info/profile.js
@@ -1,21 +1,37 @@
 exports.run = async (client, message, args) => {
     const target = message.author;
-
     const profile = await client.managers.profiles.getOrCreateProfile(target.id);
-    message.channel.send({
-        embed: global.factory.embed()
-            .setTitle(target.tag)
-            .addField('Money:', `$${profile.money}`)
-            .addField('XP:', `${profile.xp.toFixed(2)}`)
-            .addField('Friends:', profile.friends.map(id => {
-                let user = client.users.get(id);
-                if (user) {
-                    return user.username;
-                } else {
-                    return '(Unknown)';
-                }
-            }).join(', ') || 'None')
-    });
+
+    if (args.length > 0) {
+        if (/^bio(graphy)?$/i.test(args[0])) {
+            let bio = args.slice(1).join(' ');
+            if (!bio) {
+                bio = null;
+            }
+
+            profile.bio = bio;
+
+            await client.managers.profiles.updateProfile(target.id, profile);
+            return message.channel.send(':white_check_mark: Updated biography.');
+        }
+    }
+
+    const embed = global.factory.embed()
+        .setDescription(profile.bio || '*This user has no biography.*')
+        .setTitle(target.tag)
+        .setThumbnail(target.avatarURL)
+        .addField('Money:', `$${profile.money}`)
+        .addField('XP:', `${profile.xp.toFixed(2)}`)
+        .addField('Friends:', profile.friends.map(id => {
+            let user = client.users.get(id);
+            if (user) {
+                return user.username;
+            } else {
+                return '(Unknown)';
+            }
+        }).join(', ') || 'None');
+
+    message.channel.send({ embed });
 };
 
 exports.info = {
